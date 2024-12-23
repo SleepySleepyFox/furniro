f2c849acdc4beb801e06a7cd9869c5ef8c0e17fe
@Yevhen.Petrikeiev <ShkrebNsh@gmail.com>
2024-12-23 09:26:21 +0200

    Hero section style fix

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -1,13 +1,17 @@
 import React from 'react'
 
 export default function Hero() {
+  // ADD container div 
+  // on bigger screens when bg-img is centered use this external div container to help with positioning other hero section ellements 
   return (
-    <div className='flex justify-center md:justify-end md:items-start lg+:items-center w-screen h-screen md:h-[717px] md:bg-hero-pattern md:bg-no-repeat md:bg-contain lg+:bg-cover lg+:h-[800px]'>
-      <div className='w-screen md:w-[400px] lg:w-[600px] md:h-[300px] lg:h-[400px] md:relative md:right-14 md:top-10 lg:top-28 lg+:top-0 md:gap-2 md:text-start md:p-4 lg:p-9 bg-[#FFF3E3] md:rounded-lg  flex flex-col justify-center text-center gap-4 px-2'>
-        <p className='text-sm font-semibold'>New Arrival</p>
-        <h1 className=' text-[#B88E2F] font-bold text-5xl md:text-2xl lg:text-5xl'>Discover Our New Collection</h1>
-        <p className='text-sm'>Our new collection offers timeless designs, blending comfort, quality, and style for every space.</p>
-        <button className='bg-[#B88E2F] h-[74px] w-full font-black text-white md:w-56'>Buy Now</button>
+    <div>
+      <div className='flex justify-center md:justify-end md:items-start lg+:items-center w-screen h-screen md:h-[717px] md:bg-auto md:bg-hero-pattern md:bg-no-repeat md:bg-center lg+:bg-cover lg+:h-[800px]'>
+        <div className='w-screen flex-shrink lg:min-h-[350px] md:h-[300px] md:w-[440px] lg:w-[600px] md:self-center lg:max-h-[400px] md:relative md:right-14 md:gap-2 md:text-start md:p-4 lg:p-9 bg-[#FFF3E3] md:rounded-lg  flex flex-col justify-center text-center gap-4 px-2'>
+          <p className='text-sm font-semibold'>New Arrival</p>
+          <h1 className=' text-[#B88E2F] font-bold text-5xl md:text-2xl lg:text-5xl'>Discover Our New Collection</h1>
+          <p className='text-sm'>Our new collection offers timeless designs, blending comfort, quality, and style for every space.</p>
+          <button className='bg-[#B88E2F] h-[74px] md:h-[54px] lg:h-[74px] w-full font-black text-white md:w-56'>Buy Now</button>
+        </div>
       </div>
     </div>
   )
