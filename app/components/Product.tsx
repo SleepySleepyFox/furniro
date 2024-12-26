dec038cff19e8b8027ac91a26e8d1c9159e4d397
@Yevhen.Petrikeiev <ShkrebNsh@gmail.com>
2024-12-26 11:40:30 +0200

    Product section responsive fix

--- app/components/Product.tsx
+++ app/components/Product.tsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import Image from 'next/image'
 
-export default function ({productName, productCost, productImg} : {productName : string, productCost : string, productImg : string}) {
+export default function ({ productName, productCost, productImg }: { productName: string, productCost: string, productImg: string }) {
   return (
-    <div className='flex flex-col h-[446px] w-[285px] border-solid bg-[#F4F5F7] border-[#F4F5F7] border-2 '>
+    <div className='flex flex-col h-[446px]  border-solid bg-[#F4F5F7] border-[#F4F5F7] border-2 justify-self-stretch'>
 
-      <div className='h-[301px] w-[300px] flex justify-center'>
-        <Image fill={true} alt={'productIMG'} src={`${productImg}`}  className='object-cover'/>
+      <div className='flex justify-center h-[301px] relative'>
+        <Image fill alt={'productIMG'} src={`${productImg}`} objectFit='cover' className='object-cover' />
       </div>
 
       <div className='bg-[#e6e8eb] h-1/3'>
--- app/components/ProductSection.tsx
+++ app/components/ProductSection.tsx
@@ -2,6 +2,7 @@
 import React, { useEffect, useState } from 'react'
 import axios from 'axios';
 import Product from './Product';
+import data from "../../mockData.json"
 
 interface productFields {
   id?: number,
@@ -14,7 +15,7 @@ interface productFields {
 }
 
 export default function ProductSection() {
-  const [displayedProducts, setDisplayedProducts] = useState<productFields[]>()
+  const [displayedProducts, setDisplayedProducts] = useState<productFields[]>(data)
   //TODO: display categories, attach images to certian categories
   // Apply filter to the next section on click 
 
@@ -34,7 +35,7 @@ export default function ProductSection() {
         'x-rapidapi-host': `${process.env.NEXT_PUBLIC_API_HOST}`
       }
     };
-    
+
     try {
       const response = await axios.request(options);
       setDisplayedProducts(response.data)
@@ -43,30 +44,31 @@ export default function ProductSection() {
     }
   }
 
-useEffect(() => {
-  getData()
-},[])
+  useEffect(() => {
+    console.log(displayedProducts)
+    //getData
+  }, [])
 
   // Display data from API
-  const apiData = displayedProducts?.slice(0,10).map(e => {
-   return <Product key={e.id} productName={e.name} productCost={e.price.currentPrice} productImg={`${e.image}`}/>
+  const apiData = displayedProducts?.slice(0, 10).map(e => {
+    return <Product key={e.id} productName={e.name} productCost={e.price.currentPrice} productImg={`${'/HeroBG.png'}`} />
   })
 
   return (
-    <div className='h-fit p-4 lg+:p-9'>
+    <div className='h-fit lg+:p-9 p-4'>
       <h1 className='text-primary_h font-bold text-center text-3xl py-8'>Our Producs</h1>
 
-      {displayedProducts ? 
-      <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 md:justify-around lg:grid-cols-3 lg+:grid-cols-5 gap-2 place-items-center'>
-      {apiData}
-    </div> :
-    <div className='flex flex-col items-center'>
-      <h1>
-      {'(︶︹︶)'}
-      </h1>
-      <p>Nothing Here</p>
-    </div>
-    }
+      {displayedProducts ?
+        <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 justify-items-center lg:grid-cols-3 lg+:grid-cols-5 gap-2 place-items-center'>
+          {apiData}
+        </div> :
+        <div className='flex flex-col items-center'>
+          <h1>
+            {'(︶︹︶)'}
+          </h1>
+          <p>Nothing Here</p>
+        </div>
+      }
     </div>
   )
 }
