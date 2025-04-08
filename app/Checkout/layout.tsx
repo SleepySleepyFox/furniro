b3eea34cba9b6af2c5f81f235ced5193975c414e
@Yevhen.Petrikeiev <ShkrebNsh@gmail.com>
2025-04-08 13:54:08 +0300

    checkout draft ui

--- app/Checkout/layout.tsx
+++ app/Checkout/layout.tsx
@@ -1,65 +1,69 @@
 import React from 'react'
 import Header from '../common/Header'
 import Navbar from '../components/Navbar'
+import ShopBaner from '../components/ShopBaner'
+import Footer from '../components/Footer'
 
 export default function layout({children} : {children : React.ReactNode}) {
   return (
-    <div>
+    <div className='w-full'>
         <Navbar/>
         <Header h='Checkout' p='p' bg='bg-shop-header'/>
-        <div className='flex w-full justify-between 2xl:px-[20%]'>
+        <div className='flex w-full justify-between md:px-[5%] 2xl:px-[20%] py-[5%] md:gap-36 lg:gap-52'>
             <div className='[&>div]:gap-2 '>
-                <h1>Billing details</h1>
+                <h1 className='font-semibold text-3xl'>Billing details</h1>
                 <div className='flex'>
                     <div className='flex flex-col'>
                         <label htmlFor="firstName">First Name</label>
-                        <input type="text" name='firstName'  className='checkout-forms'/>
+                        <input type="text" name='firstName' id='firstName'  className='checkout-forms'/>
                     </div>
                     
                     <div className='flex flex-col'>
-                        <label htmlFor="secondName">First Name</label>
-                        <input type="text" name='secondName' className='checkout-forms' />
+                        <label htmlFor="secondName">Second Name</label>
+                        <input type="text" name='secondName' id='secondName' className='checkout-forms' />
                     </div>
                 </div>
 
                 <div className='flex flex-col'>
                     <label htmlFor="companyName">{"Company Name (Optional)"}</label>
-                    <input type="text" name='companyName' className='checkout-forms' />
+                    <input type="text" name='companyName' id='companyName' className='checkout-forms' />
                 </div>
 
                 <div className='flex flex-col'>
                     <label htmlFor="region">Country / Region</label>
-                    <input type="text" name='region' className='checkout-forms' />
+                    <input type="text" name='region' id='region' className='checkout-forms' />
                 </div>
 
                 <div className='flex flex-col'>
                     <label htmlFor="city">Town / City</label>
-                    <input type="text" name='city' className='checkout-forms' />
+                    <input type="text" name='city' id='city' className='checkout-forms' />
                 </div>
 
                 <div className='flex flex-col'>
-                    <label htmlFor="addres">Street address</label>
-                    <input type="text" name='addres' className='checkout-forms' />
+                    <label htmlFor="address">Street address</label>
+                    <input type="text" name='address' id='address' className='checkout-forms' />
                 </div>
 
                 <div className='flex flex-col'>
                     <label htmlFor="zipcode">ZIP code</label>
-                    <input type="text" name='zipcode' className='checkout-forms' />
+                    <input type="text" name='zipcode' id='zipcode' className='checkout-forms' />
                 </div>
 
                 <div className='flex flex-col'>
                     <label htmlFor="phone">Phone</label>
-                    <input type="number" name='phone' className='checkout-forms' />
+                    <input type="tel" name='phone' id='phone' className='checkout-forms' />
                 </div>
 
                 <div className='flex flex-col'>
                     <label htmlFor="email">Email</label>
-                    <input type="email" name='email' className='checkout-forms' />
+                    <input type="email" name='email' id='email' className='checkout-forms' />
                 </div>
                 
             </div>
             {children}
         </div>
+        <ShopBaner/>
+        <Footer/>
     </div>
   )
 }
--- app/Checkout/page.tsx
+++ app/Checkout/page.tsx
@@ -2,6 +2,50 @@ import React from 'react'
 
 export default function page() {
   return (
-    <div>Product</div>
+    <div className='w-[600px]'>
+      <div className='py-6'>
+        <div className='w-full flex justify-between'>
+          <h1 className='text-2xl'>Product</h1>
+          <h1 className='text-2xl'>Subtotal</h1>
+        </div>
+
+        <div className='w-full flex justify-between'>
+          <p className='text-[#9F9F9F]'>ProductName <span className='text-black'>x1</span></p>
+          <p>250.000</p>
+        </div>
+
+        <div className='w-full flex justify-between'>
+          <p>Subtotal</p>
+          <p>250.000</p>
+        </div>
+
+        <div className='w-full flex justify-between bp-6'>
+          <p>Total</p>
+          <p className='text-primary font-bold text-2xl'>250.000.00</p>
+        </div>
+      </div>
+
+      <hr className='w-full' />
+
+      <div className='py-4'>
+        <p className='font-light text-[#9F9F9F]'>Make your payment directly into our bank account. Please use your Order ID as the payment reference. Your order will not be shipped until the funds have cleared in our account.</p>
+
+        <form className='text-[#9F9F9F] py-2'>
+         
+          <input type="radio" id='bank' name='payment' className='accent-black'/>
+          <label htmlFor="bank" className='ml-2'>Direct Bank Transfer</label>
+          <br />
+          <input type="radio" id='cash' name='payment' className='accent-black'/>
+          <label htmlFor="cash" className='ml-2'>Cash On Delivery</label>
+          
+        </form>
+
+        <p className='font-light'>Your personal data will be used to support your experience throughout this website, to manage access to your account, and for other purposes described in <a href=""><strong className='font-bold'>our privacy policy.</strong></a></p>
+
+        <div className='w-full flex justify-center pt-6'>
+          <button className='h-[64px] w-[318px] border-[1px] border-black rounded-xl'>Place order</button>
+        </div>
+      </div>
+    </div>
   )
 }
