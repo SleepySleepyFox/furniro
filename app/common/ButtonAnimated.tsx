783a28f96edf113bf42154cce682db3989e3baad
@Yevhen.Petrikeiev <ShkrebNsh@gmail.com>
2025-04-10 14:29:15 +0300

    basic animation added

--- app/common/ButtonAnimated.tsx
+++ app/common/ButtonAnimated.tsx
@@ -1,12 +1,58 @@
 'use client'
-import React, { useState } from 'react'
+import { useGSAP } from '@gsap/react'
+import gsap from 'gsap'
+import React, { useEffect, useRef } from 'react'
 
 export default function ButtonAnimated() {
-// TODO: Animation (GSAP?)
+const divRef = useRef(null)
+const buttonRef = useRef<HTMLButtonElement>(null)
+
+console.log(buttonRef)
+
+const animationIn = (e: React.MouseEvent) => {
+  const bounds = buttonRef.current?.getBoundingClientRect()
+  if(!bounds) return
+  const posX = e.clientX - bounds.left 
+  const posY = e.clientY - bounds.bottom 
+  const tl = gsap.timeline()
+  tl.to(divRef.current, {scale: 100, ease:"power3.inOut"}, 0.01)
+  tl.to(buttonRef.current, {color: "#FFF", immediateRender: true}, 0.01)
+  tl.to(divRef.current, {x: posX, y: posY}, 0)
+}
+
+const animationOut = (e: React.MouseEvent) => {
+  const bounds = buttonRef.current?.getBoundingClientRect()
+  if(!bounds) return
+  const posX = e.clientX - bounds.left
+  const posY = e.clientY - bounds.bottom
+
+  const tl = gsap.timeline()
+  tl.to(divRef.current, {scale: 0}, 0.01)
+  tl.to(divRef.current, {x: posX, y: posY}, 0.01)
+
+  tl.to(buttonRef.current, {color: "#000", immediateRender: true})
+}
+const posAdjust = (e: React.MouseEvent) => {
+  const bounds = buttonRef.current?.getBoundingClientRect()
+  if(!bounds) return
+  const posX = e.clientX - bounds.left
+  const posY = e.clientY - bounds.bottom 
+
+  const tl = gsap.timeline()
+  tl.to(divRef.current, {x: posX, y: posY}, 0.01)
+
+}
+
   return (
     <button 
-    className='h-[64px] w-[318px] border-[1px] border-black rounded-xl relative overflow-clip bg-white hover:bg-black hover:text-white duration-300'>
+    ref={buttonRef}
+    onMouseEnter={(e) => animationIn(e)}
+    onMouseLeave={(e) => animationOut(e)}
+    onMouseMove={(e) => posAdjust(e)}
+    className='h-[64px] w-[318px] border-[1px] border-black rounded-xl relative text-black  overflow-clip'>  
         Place order
+       
+        <div ref={divRef} className='w-3 h-3 scale-0 rounded-full bg-black absolute divRef aspect-square -z-10'></div>
     </button>
   )
 }
