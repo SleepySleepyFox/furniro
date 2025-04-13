a7c337c9449e748fcfa6ee758d6eae2d7260c7eb
@Yevhen.Petrikeiev <ShkrebNsh@gmail.com>
2025-04-13 20:04:57 +0300

    display some data in product page

--- app/Products/[productsId]/page.tsx
+++ app/Products/[productsId]/page.tsx
@@ -1,32 +1,28 @@
 'use client'
 import Breadcrumbs from '@/app/common/Breadcrumbs';
+import ProductPhotos from '@/app/components/ProductPhotos';
 import { productFields } from '@/app/types/productTypes';
 import { notFound } from 'next/navigation';
 import { useRouter } from 'next/navigation';
 import React from 'react'
 
 export default function page({params, searchParams} : {params : {productsId : string}, searchParams : {productData : string, path: string}}) {
-   
-    // Horrible but works, fix later!
-    // try{
-    //     const dataTest = JSON.parse(searchParams.productData)
-    //     // if(dataTest.id != params.productsId) notFound()
-    // }catch(e){  
-    //     notFound()
-    // }
-    // const ls = localStorage.getItem("productData")
-    // if(!ls) return
-    // const data : productFields = JSON.parse(ls)
     const getLocalStorage = localStorage.getItem("productData")
     if(!getLocalStorage) return
     const data : productFields = JSON.parse(getLocalStorage)
+    console.log(data)
     return (
-    <div>   
+    <div className='flex justify-center flex-col p-6'>   
         {/* <Breadcrumbs path={searchParams.path} item={data.name}/> */}
 
-        <h1>
-        {data.name}   
-        </h1>
+        <ProductPhotos/>
+        <div>
+            <h1 className='text-3xl'>
+                {data.name}   
+            </h1>
+            <p className='text-[#9F9F9F]'>{data.price}$</p>
+            <p>{data.description}</p>
+        </div>
     </div>
   )
 }
--- app/components/ProductPhotos.tsx
+++ app/components/ProductPhotos.tsx
@@ -1,7 +1,17 @@
 import React from 'react'
+import { productFields } from '../types/productTypes'
 
 export default function ProductPhotos() {
+
+  const ls = localStorage.getItem("productData")
+  if(!ls) return
+  const data : productFields = JSON.parse(ls)
+  console.log('photoData: ', data)
   return (
-    <div>ProductPhotos</div>
+    <div>
+      <div>
+        <img src={data.image_path} alt="" />
+      </div>
+    </div>
   )
 }
