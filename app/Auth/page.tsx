49a1cfbcba9f88f5675fe0d4f4abe1f6ac72b360
@Yevhen.Petrikeiev <ShkrebNsh@gmail.com>
2025-04-26 19:31:07 +0300

    basic registration

--- app/Auth/page.tsx
+++ app/Auth/page.tsx
@@ -2,11 +2,27 @@
 import React, { useState } from 'react'
 import ButtonAnimated from '../common/ButtonAnimated'
 import Button from '../common/Button';
+import { UserData } from '../types/UserData';
+import axios from 'axios';
 
 export default function page() {
-    const [email, setEmail] = useState('')
-    const [password, setPassword] = useState('')
-    const [repeatPassword, setRepeatPassword] = useState('')
+    const [FirstName, setFirstName] = useState('')
+    const [SecondName, setSecondName] = useState('')
+    const [Email, setEmail] = useState('')
+    const [Password, setPassword] = useState('')
+    const [RepeatPassword, setRepeatPassword] = useState('')
+
+    const sendData = () => {
+        if(!process.env.NEXT_PUBLIC_FURNIRO_BACKEND) return 
+        axios.post(`${process.env.NEXT_PUBLIC_FURNIRO_BACKEND}api/register`,{
+            FirstName: FirstName,
+            SecondName: SecondName,
+            Email: Email,
+            Password: Password
+        })
+        // console.log(FirstName,SecondtName,Email,Password)
+    }
+
 
   return (
     <div className='h-screen w-full flex justify-center items-center'>
@@ -15,10 +31,12 @@ export default function page() {
                 <img src="/logo.svg" alt="" />
             </div>
             <form action="" className='flex flex-col p-6 gap-4'>
+                <input type="text"  placeholder='First Name' className='checkout-forms w-full' onChange={(e) => setFirstName(e.target.value)}/>
+                <input type="text"  placeholder='Second Name' className='checkout-forms w-full' onChange={(e) => setSecondName(e.target.value)}/>
                 <input type="email"  placeholder='Email' className='checkout-forms w-full' onChange={(e) => setEmail(e.target.value)}/>
                 <input type="password" placeholder='Password' className='checkout-forms w-full'  onChange={(e) => setPassword(e.target.value)}/>
                 <input type="password" placeholder='Repeat Passwprd' className='checkout-forms w-full' onChange={(e) => setRepeatPassword(e.target.value)}/>
-                <Button color='bg-primary' text='Register' textColor='text-white' size='w-full p-4' addStyle='rounded-md'/>
+                <Button color='bg-primary' text='Register' textColor='text-white' size='w-full p-4' addStyle='rounded-md' onClick={sendData}/>
             </form>
         </div>
     </div>
